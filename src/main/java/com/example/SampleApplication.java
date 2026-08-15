1fb8763641a96585e770d94e1f310f04708ff9df
krishsam94 <krishsam94@gmail.com>
2026-08-15 16:31:48 +0530

    Update SampleApplication.java

--- src/main/java/com/example/SampleApplication.java
+++ src/main/java/com/example/SampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleApplication {
     public static void main(String[] args) {
+        System.out.println("Application Starting...");
         SpringApplication.run(SampleApplication.class, args);
     }
 }
